76a008a13e7bac99d94e535d3b7e4764f74aab34
Devendra Negi <devendra.singhnegi@walmartlabs.com>
2019-10-17 12:43:13 +0530

    Initial project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Babel setup
+yar add @babel/cli @babel/core @babel/node @babel/register @babel/polyfill --dev
+yarn add @babel/preset-env --dev
+yarn add husky --dev
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "@babel/polyfill";
+import http from "http";
+
+const requestHandler = function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello, World!');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(8080);
